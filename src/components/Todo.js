157b7c368a2ba9a5409c7dca1e1556f9f233ac4e
kjx63 <ueyama.kenji.63@gmail.com>
2020-09-02 22:57:00 +0900

    Add Text when there's no todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,27 +6,37 @@ import { Button } from '@material-ui/core';
 const Todo = ({ todos, deleteTodo, completeTodo }) => {
   return (
     <div className='todo'>
-      {todos.map((todo, index) => (
-        <div className='todo__list' key={index}>
-          <p
-            className={todo.isCompleted ? 'todo__isCompleted' : ''}
-            onClick={() => {
-              completeTodo(index);
-            }}
-          >
-            {todo.text}
-          </p>
-          <div className='todo__button'>
-            <Button
-              onClick={() => deleteTodo(index)}
-              variant='contained'
-              color='secondary'
-            >
-              Delete
-            </Button>
-          </div>
+      {todos.length == 0 ? (
+        <p>
+          There is no Todo at the moment.
+          <br />
+          Please Add your Todo...
+        </p>
+      ) : (
+        <div>
+          {todos.map((todo, index) => (
+            <div className='todo__list' key={index}>
+              <p
+                className={todo.isCompleted ? 'todo__isCompleted' : ''}
+                onClick={() => {
+                  completeTodo(index);
+                }}
+              >
+                {todo.text}
+              </p>
+              <div className='todo__button'>
+                <Button
+                  onClick={() => deleteTodo(index)}
+                  variant='contained'
+                  color='secondary'
+                >
+                  Delete
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 };
